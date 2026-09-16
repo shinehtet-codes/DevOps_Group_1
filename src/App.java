e9537ac8f6ac6eb952548e2d8b22189f669b2977
Christina <mzaw28549@gmail.com>
2026-09-16 21:26:58 +0630

    Add new app

---
+++ src/App.java
@@ -0,0 +1,9 @@
+// Every Java file must have a class that matches the filename.
+public class HelloWorld {
+
+    // The main method is the entry point where the program starts running.
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the terminal window.
+        System.out.println("Hello, World!");
+    }
+}
