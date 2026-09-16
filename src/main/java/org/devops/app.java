96659f8b14676bbec59ab235a0d491f0772bcbb6
Christina <mzaw28549@gmail.com>
2026-09-16 21:34:07 +0630

    changes app

--- src/main/java/org/devops/app.java
+++ src/main/java/org/devops/app.java
@@ -1,4 +1,12 @@
 package org.devops;
 
-public class app {
+// Every Java file must have a class that matches the filename.
+public class app{
+
+    // The main method is the entry point where the program starts running.
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the terminal window.
+        System.out.println("Hello, World!");
+    }
 }
+
